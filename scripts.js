fe174bbe1ec97dc477de2be2c5a8bc71b0401267
Rodrigo Sul <44475254+sulrodrigo@users.noreply.github.com>
2021-06-22 00:55:15 -0300

    Alterações bugadas

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+pontos = 0;
+
 const draggables = document.querySelectorAll('.draggable');
 const contents = document.querySelectorAll('.content');
 
@@ -63,35 +65,35 @@ var question_two = document.getElementById('question-2');
 var question_three = document.getElementById('question-3');
 var question_four = document.getElementById('question-4');
 var question_five = document.getElementById('question-5');
-// var question_six = document.getElementById('question-6');
+var question_six = document.getElementById('question-6');
 // var question_seven = document.getElementById('question-7');
 // var question_eight = document.getElementById('question-8');
 // var question_nine = document.getElementById('question-9');
 // var question_ten = document.getElementById('question-10');
 
-function storeAnswer(question_number, event){
-    if(event.target.type === 'radio'){
-        console.log(event.target.value);
-        answers['question'+question_number] = parseInt(event.target.value);
-        console.log(answers);
-    }
-}
+// function storeAnswer(question_number, event){
+//     if(event.target.type === 'radio'){
+//         console.log(event.target.value);
+//         answers['question'+question_number] = parseInt(event.target.value);
+//         console.log(answers);
+//     }
+// }
 
-question_one.addEventListener('click', function(event){
-    storeAnswer(1, event)
-})
-question_two.addEventListener('click', function(event){
-    storeAnswer(2, event)
-})
-question_three.addEventListener('click', function(event){
-    storeAnswer(3, event)
-})
-question_four.addEventListener('click', function(event){
-    storeAnswer(4, event)
-})
-question_five.addEventListener('click', function(event){
-    storeAnswer(5, event)
-})
+// question_one.addEventListener('click', function(event){
+//     storeAnswer(1, event)
+// })
+// question_two.addEventListener('click', function(event){
+//     storeAnswer(2, event)
+// })
+// question_three.addEventListener('click', function(event){
+//     storeAnswer(3, event)
+// })
+// question_four.addEventListener('click', function(event){
+//     storeAnswer(4, event)
+// })
+// question_five.addEventListener('click', function(event){
+//     storeAnswer(5, event)
+// })
 // question_six.addEventListener('click', function(event){
 //     storeAnswer(6, event)
 // })
@@ -108,38 +110,38 @@ question_five.addEventListener('click', function(event){
 //     storeAnswer(10, event)
 // })
 
-function totalScore(){
-    var total_score = 
-    answers.question1+
-    answers.question2+
-    answers.question3+
-    answers.question4+ 
-    answers.question5;
-    // answers.question6+
-    // answers.question7+
-    // answers.question8+
-    // answers.question9+
-    // answers.question10;
+// function totalScore(){
+//     var total_score = 
+//     answers.question1+
+//     answers.question2+
+//     answers.question3+
+//     answers.question4+ 
+//     answers.question5+
+//     answers.question6;
+//     answers.question7+
+//     answers.question8+
+//     answers.question9+
+//     answers.question10;
     
-    return total_score;
-}
+//     return total_score;
+// }
 
-function getInfoBasedOnScore(){
-    if(totalScore() < 7){
-        var score_info = "Você acertou menos de 7 questões :(";
-    } else if(totalScore() > 7){
-        var score_info = "Parabéns! Você acertou mais que a média!"
-    }
+// function getInfoBasedOnScore(){
+//     if(totalScore() < 7){
+//         var score_info = "Você acertou menos de 7 questões :(";
+//     } else if(totalScore() > 7){
+//         var score_info = "Parabéns! Você acertou mais que a média!"
+//     }
 
-    return score_info;
-}
+//     return score_info;
+// }
 
 var submit1 = document.getElementById('submit1');
 var submit2 = document.getElementById('submit2');
 var submit3 = document.getElementById('submit3');
 var submit4 = document.getElementById('submit4');
 var submit5 = document.getElementById('submit5');
-// var submit6 = document.getElementById('submit6');
+var submit6 = document.getElementById('submit6');
 // var submit7 = document.getElementById('submit7');
 // var submit8 = document.getElementById('submit8');
 // var submit9 = document.getElementById('submit9');
@@ -177,10 +179,10 @@ submit5.addEventListener('click', function(){
     nextQuestion(6);
     growProgressBar('50%');
 })
-// submit6.addEventListener('click', function(){
-//     nextQuestion(7);
-//     growProgressBar('60%');
-// })
+submit6.addEventListener('click', function(){
+    nextQuestion(7);
+    growProgressBar('60%');
+})
 // submit7.addEventListener('click', function(){
 //     nextQuestion(8);
 //     growProgressBar('70%');
@@ -197,10 +199,10 @@ submit5.addEventListener('click', function(){
 //     nextQuestion(11);
 // })
 
-submit5.addEventListener('click', function(){
-    document.getElementById("printtotalscore").innerHTML = totalScore();
-    document.getElementById("printscoreinfo").innerHTML = getInfoBasedOnScore();
-})
+// // submit5.addEventListener('click', function(){
+// //     document.getElementById("printtotalscore").innerHTML = totalScore();
+// //     document.getElementById("printscoreinfo").innerHTML = getInfoBasedOnScore();
+// // })
 
 function growProgressBar(percentage_width){
     var bar = document.getElementById("progress_bar");
@@ -213,39 +215,74 @@ function alert_resultado1() {
 
     if(Number(response) == 1) {
         alert("Parabéns! Você acertou a questão!");
+        pontos++;
+        console.log(pontos);
+    } else {
+        alert("Você errou a questão :( Mas vamos pra próxima!");
+        console.log(pontos);
+    }
+}
+
+function alert_resultado2() {
+    const id = document.getElementById('correct')
+    const response = id.childNodes[3].getAttribute('aria-valuetext')
+
+    console.log(response)
+
+    if(Number(response) == 1) {
+        alert("Parabéns! Você acertou a questão!");
+        pontos++;
+        console.log(pontos);
+    } else {
+        alert("Você errou a questão :( Mas vamos pra próxima!");
+        console.log(pontos);
+    }
+}
+
+function alert_resultado3() {
+    const id = document.getElementById('correct')
+    const response = id.childNodes[3].getAttribute('aria-valuetext')
+
+    if(Number(response) == 1) {
+        alert("Parabéns! Você acertou a questão!");
     } else {
         alert("Você errou a questão :( Mas vamos pra próxima!");
     }
 }
 
-function alert_resultado2() {
-    if(answers.question2 == 1) {
-        alert("Parabéns! Você acertou a questão!");
-    } else {
-        alert("Você errou a questão :( Mas vamos pra próxima!");
-    }
-}
-function alert_resultado3() {
-    if(answers.question3 == 1) {
-        alert("Parabéns! Você acertou a questão!");
-    } else {
-        alert("Você errou a questão :( Mas vamos pra próxima!");
-    }
-}
 function alert_resultado4() {
-    if(answers.question4 == 1) {
+    const id = document.getElementById('correct')
+    const response = id.childNodes[3].getAttribute('aria-valuetext')
+
+    if(Number(response) == 1) {
         alert("Parabéns! Você acertou a questão!");
     } else {
         alert("Você errou a questão :( Mas vamos pra próxima!");
     }
 }
+
 function alert_resultado5() {
-    if(answers.question5 == 1) {
+    const id = document.getElementById('correct')
+    const response = id.childNodes[3].getAttribute('aria-valuetext')
+
+    if(Number(response) == 1) {
         alert("Parabéns! Você acertou a questão!");
     } else {
         alert("Você errou a questão :( Mas vamos pra próxima!");
     }
 }
+
+function alert_resultado6() {
+    const id = document.getElementById('correct')
+    const response = id.childNodes[3].getAttribute('aria-valuetext')
+
+    if(Number(response) == 1) {
+        alert("Parabéns! Você acertou a questão!");
+    } else {
+        alert("Você errou a questão :( Mas vamos pra próxima!");
+    }
+}
+
 
 function alert_gabarito1() {
     alert("Como o livro se encontra em equilíbrio sobre a mesa, a força total que atua sobre ele é zero. Através da força resultante, temos:\n" + "Fr = m . a\n" + "F - P = 0\n" + "F = P => F = 10N");
@@ -265,4 +302,8 @@ function alert_gabarito4() {
 
 function alert_gabarito5() {
     alert("Para calcularmos a aceleração média do ponto material, utilizaremos a equação da aceleração média. De acordo com o texto, o móvel partiu do repouso, logo, sua velocidade inicial era 0 e sua velocidade final foi de 30 m/s, dessa forma, devemos resolver o seguinte cálculo:\n a = 30 - 0 / 5 \n a = 6 m/s²");
+}
+
+function alert_gabarito6() {
+    alert("Aplicando a segunda lei de Newton, temos: \nFʀ = m . a  >>  N - P = m . a \nN - m . g = m . a  >>  N = m . a + m . g \nN = m (a + g)  >>  N = 10 (0,1 + 10) \nN = 10 . 10,1 = 101 N");
 }
